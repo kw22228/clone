a5852c3d1e4571a858d4b31a25f98f27c81792be
kw22228 <hot5521@naver.com>
2022-05-06 17:58:30 +0900

    init

---
+++ momentum/app.js
@@ -0,0 +1,12 @@
+const loginForm = document.querySelector('#login-form');
+const loginInput = loginForm.querySelector('input');
+const loginBtn = loginForm.querySelector('button');
+
+// loginBtn.addEventListener('click', () => {
+//     const username = loginInput.value;
+//     if (!username) {
+//         console.log('Please write value.');
+//     } else if (username.length > 15) {
+//         console.log('Your name is too long.');
+//     }
+// });
