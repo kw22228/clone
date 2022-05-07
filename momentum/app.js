feffc75e4739f69e824eca5a35cad21f74f7c0b8
김재원 <kw2282222@gmail.com>
2022-05-07 19:20:57 +0900

    login

--- momentum/app.js
+++ momentum/app.js
@@ -1,12 +1,33 @@
 const loginForm = document.querySelector('#login-form');
 const loginInput = loginForm.querySelector('input');
-const loginBtn = loginForm.querySelector('button');
+const greeting = document.querySelector('#greeting');
 
-// loginBtn.addEventListener('click', () => {
-//     const username = loginInput.value;
-//     if (!username) {
-//         console.log('Please write value.');
-//     } else if (username.length > 15) {
-//         console.log('Your name is too long.');
-//     }
-// });
+const HIDDEN_CLASSNAME = 'hidden';
+const USERNAME_KEY = 'username';
+
+function isLogin() {
+    const savedUserName = localStorage.getItem(USERNAME_KEY);
+
+    if (savedUserName) {
+        loginForm.classList.add(HIDDEN_CLASSNAME);
+        paintGreetings(savedUserName);
+    } else {
+        loginForm.addEventListener('submit', e => {
+            e.preventDefault();
+
+            loginForm.classList.add(HIDDEN_CLASSNAME);
+
+            const username = loginInput.value;
+            localStorage.setItem(USERNAME_KEY, username);
+
+            paintGreetings(username);
+        });
+    }
+}
+
+function paintGreetings(username) {
+    greeting.innerText = `Hello ${username}`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+}
+
+window.addEventListener('DOMContentLoaded', isLogin);
