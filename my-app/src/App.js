74e9789e58db95b7fa5260e45e37c25500abd53c
kw22228 <hot5521@naver.com>
2022-05-12 15:41:30 +0900

    update

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,27 +2,42 @@ import { useState, useEffect, useMemo } from 'react';
 import Button from './Button';
 import styles from './App.module.css';
 
+function Hello() {
+    useEffect(() => {
+        console.log('Mount');
+
+        return () => console.log('Destroy');
+    }, []);
+    return <h1>Hello</h1>;
+}
+
 function App() {
-    const [counter, setCounter] = useState(0);
-    const [keyword, setKeyword] = useState('');
-    const onClick = () => setCounter(prev => prev + 1);
-    const onChange = event => setKeyword(event.target.value);
+    // const [counter, setCounter] = useState(0);
+    // const [keyword, setKeyword] = useState('');
+    // const onClick = () => setCounter(prev => prev + 1);
+    // const onChange = event => setKeyword(event.target.value);
+    // useEffect(() => {
+    //     if (keyword !== '') {
+    //         console.log('Search for ', keyword);
+    //     }
+    // }, [keyword]);
+    // useEffect(() => {
+    //     console.log('Counter is ', counter);
+    // }, [counter]);
+    // return (
+    //     <div>
+    //         <input type="text" placeholder="Search here" onChange={onChange} value={keyword} />
+    //         <h1 className={styles.title}>{counter}</h1>
+    //         <Button text="Continue" onClick={onClick} />
+    //     </div>
+    // );
 
-    useEffect(() => {
-        if (keyword !== '') {
-            console.log('Search for ', keyword);
-        }
-    }, [keyword]);
-
-    useEffect(() => {
-        console.log('Counter is ', counter);
-    }, [counter]);
-
+    const [show, setShow] = useState(false);
+    const onClick = () => setShow(prev => !prev);
     return (
         <div>
-            <input type="text" placeholder="Search here" onChange={onChange} value={keyword} />
-            <h1 className={styles.title}>{counter}</h1>
-            <Button text="Continue" onClick={onClick} />
+            {show && <Hello />}
+            <button onClick={onClick}>{show ? 'Hide' : 'Show'}</button>
         </div>
     );
 }
--- my-app/src/Button.js
+++ my-app/src/Button.js
@@ -1,11 +1,14 @@
+import { useState } from 'react';
 import PropTypes from 'prop-types';
 import styles from './Button.module.css';
 
 export default function Button({ text, onClick }) {
     return (
-        <button className={styles.btn} onClick={onClick}>
-            {text}
-        </button>
+        <>
+            <button className={styles.btn} onClick={onClick}>
+                {text}
+            </button>
+        </>
     );
 }
 
