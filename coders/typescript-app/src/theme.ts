fe54545df110150dda7ba097c358fdc3e5c26e2a
kw22228 <hot5521@naver.com>
2022-06-02 16:51:01 +0900

    add: global theme

---
+++ coders/typescript-app/src/theme.ts
@@ -0,0 +1,15 @@
+import { DefaultTheme } from 'styled-components';
+
+export const lightTheme: DefaultTheme = {
+    bgColor: 'white',
+    textColor: 'black',
+    btnColor: 'teal',
+    borderColor: 'black',
+};
+
+export const darkTheme: DefaultTheme = {
+    bgColor: 'black',
+    textColor: 'white',
+    btnColor: 'tomato',
+    borderColor: 'white',
+};
