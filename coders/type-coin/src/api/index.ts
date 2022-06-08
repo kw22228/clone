ba7003f02a546f83fa371ddfd508395796738ac1
kw22228 <hot5521@naver.com>
2022-06-08 16:22:47 +0900

    add: fetch api

--- coders/type-coin/src/api/index.ts
+++ coders/type-coin/src/api/index.ts
@@ -1,1 +1,25 @@
 import axios from 'axios';
+
+const BASE_URL = 'https://api.coinpaprika.com/v1';
+
+const getQuery = (path: string) => `${BASE_URL}/${path}`;
+
+export async function fetchData(path: string) {
+    const url = getQuery(path);
+    const response = await axios.get(url);
+    return response.data;
+}
+
+export async function fetchDataSlice(path: string, slice: number) {
+    const url = getQuery(path);
+    const response = await axios.get(url);
+    return response.data.slice(0, slice);
+}
+
+// export async function multiFetchData(fetchList: string[]) {
+//     axios.all(fetchList.map(f => axios.get(getQuery(f)))).then(
+//         axios.spread((...args) => {
+//             return args;
+//         })
+//     );
+// }
