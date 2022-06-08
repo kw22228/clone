b1ccf8573818582bdcee8c49269d362dc38f7e4d
kw22228 <hot5521@naver.com>
2022-06-08 16:23:09 +0900

    add: react-query

--- coders/type-coin/src/components/CoinDetail/CoinDetail.style.ts
+++ coders/type-coin/src/components/CoinDetail/CoinDetail.style.ts
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 
 export const Wrapper = styled.div`
@@ -12,7 +13,7 @@ export const RoundBox = styled.div<{ between?: boolean }>`
     align-items: center;
     background-color: ${props => props.theme.textColor};
     width: 100%;
-    padding: 20px 10px;
+    padding: 20px 0px;
     border-radius: 20px;
     margin-top: 20px;
 `;
@@ -28,4 +29,30 @@ export const Item = styled.div`
 export const Description = styled.div`
     margin-top: 20px;
     color: ${props => props.theme.textColor};
+`;
+
+//tabs
+export const TabWrapper = styled.div`
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    margin: 20px 0px;
+    gap: 20px;
+`;
+
+export const Tab = styled.div<{ isActive?: boolean }>`
+    background-color: ${props =>
+        props.isActive ? props.theme.accentColor : props.theme.textColor};
+    color: ${props => (props.isActive ? props.theme.textColor : props.theme.bgColor)};
+
+    font-weight: bold;
+    text-align: center;
+    border-radius: 100px;
+    padding: 10px 0px;
+    transition: all 0.3s ease-in-out;
+
+    a {
+        display: block;
+        width: 100%;
+        height: 100%;
+    }
 `;
--- coders/type-coin/src/components/CoinDetail/CoinDetail.tsx
+++ coders/type-coin/src/components/CoinDetail/CoinDetail.tsx
@@ -1,59 +1,64 @@
-import { useEffect, useState } from 'react';
 import { Loading } from '../Loading';
 import { ICoinInfo, ICoinPrice, IProps } from './types';
 import * as s from './CoinDetail.style';
+import { Link, useMatch } from 'react-router-dom';
+import { useQuery } from 'react-query';
+import { fetchData } from '../../api';
 
 const CoinDetail = ({ coinId }: IProps) => {
-    const [loading, setLoading] = useState(true);
-    const [coinInfoData, setCoinInfoData] = useState<ICoinInfo>();
-    const [coinPriceData, setCoinPriceData] = useState<ICoinPrice>();
+    const priceMatch = useMatch('/:coinId/price');
+    const chartMatch = useMatch('/:coinId/chart');
 
-    useEffect(() => {
-        (async () => {
-            const infoData = await (
-                await fetch(`https://api.coinpaprika.com/v1/coins/${coinId}`)
-            ).json();
-            const priceData = await (
-                await fetch(`https://api.coinpaprika.com/v1/tickers/${coinId}`)
-            ).json();
+    const { isLoading: detailLoading, data: detailData } = useQuery<ICoinInfo | undefined>(
+        ['detail', coinId],
+        () => fetchData(`coins/${coinId}`)
+    );
+    const { isLoading: priceLoading, data: priceData } = useQuery<ICoinPrice | undefined>(
+        ['price', coinId],
+        () => fetchData(`tickers/${coinId}`)
+    );
 
-            setCoinInfoData(infoData);
-            setCoinPriceData(priceData);
-            setLoading(false);
-            console.log(priceData);
-        })();
-    }, [coinId]);
-
-    return loading ? (
+    return detailLoading || priceLoading ? (
         <Loading />
     ) : (
-        <s.Wrapper>
-            <s.RoundBox>
-                <s.Item>
-                    <div>Rank</div>
-                    <div>{coinInfoData?.rank}</div>
-                </s.Item>
-                <s.Item>
-                    <div>SYMBOL</div>
-                    <div>{coinInfoData?.symbol}</div>
-                </s.Item>
-                <s.Item>
-                    <div>OPEN SOURCE</div>
-                    <div>{coinInfoData?.open_source ? 'YES' : 'NO'}</div>
-                </s.Item>
-            </s.RoundBox>
-            <s.Description>{coinInfoData?.description}</s.Description>
-            <s.RoundBox between={false}>
-                <s.Item>
-                    <div>TOTAL SUPLY</div>
-                    <div>{coinPriceData?.total_supply}</div>
-                </s.Item>
-                <s.Item>
-                    <div>MAX SUPLY</div>
-                    <div>{coinPriceData?.max_supply}</div>
-                </s.Item>
-            </s.RoundBox>
-        </s.Wrapper>
+        <>
+            <s.Wrapper>
+                <s.RoundBox>
+                    <s.Item>
+                        <div>Rank</div>
+                        <div>{detailData?.rank}</div>
+                    </s.Item>
+                    <s.Item>
+                        <div>SYMBOL</div>
+                        <div>{detailData?.symbol}</div>
+                    </s.Item>
+                    <s.Item>
+                        <div>OPEN SOURCE</div>
+                        <div>{detailData?.open_source ? 'YES' : 'NO'}</div>
+                    </s.Item>
+                </s.RoundBox>
+                <s.Description>{detailData?.description}</s.Description>
+                <s.RoundBox between={false}>
+                    <s.Item>
+                        <div>TOTAL SUPLY</div>
+                        <div>{priceData?.total_supply}</div>
+                    </s.Item>
+                    <s.Item>
+                        <div>MAX SUPLY</div>
+                        <div>{priceData?.max_supply}</div>
+                    </s.Item>
+                </s.RoundBox>
+            </s.Wrapper>
+
+            <s.TabWrapper>
+                <s.Tab isActive={chartMatch ? true : false}>
+                    <Link to={`/${coinId}/chart`}>Chart</Link>
+                </s.Tab>
+                <s.Tab isActive={priceMatch ? true : false}>
+                    <Link to={`/${coinId}/price`}>Price</Link>
+                </s.Tab>
+            </s.TabWrapper>
+        </>
     );
 };
 
