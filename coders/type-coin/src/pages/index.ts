13cfafc15267366522b8728b57514055fedceb97
kw22228 <hot5521@naver.com>
2022-06-08 16:23:59 +0900

    add: chart, price page

--- coders/type-coin/src/pages/index.ts
+++ coders/type-coin/src/pages/index.ts
@@ -1,2 +1,4 @@
 export { default as CoinsPage } from './CoinsPage';
 export { default as CoinDetailPage } from './CoinDetailPage';
+export { default as PricePage } from './PricePage';
+export { default as ChartPage } from './ChartPage';
