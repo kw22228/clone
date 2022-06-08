fd118bf482761f2aaa42aed994b855470817c5b0
kw22228 <hot5521@naver.com>
2022-06-08 16:24:28 +0900

    add: react-query devtool

--- coders/type-coin/src/index.tsx
+++ coders/type-coin/src/index.tsx
@@ -3,12 +3,20 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { ThemeProvider } from 'styled-components';
 import { theme } from './styles/theme';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
+
+//react-query initialize
+const queryClient = new QueryClient();
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
     // <React.StrictMode>
-    <ThemeProvider theme={theme.darkTheme}>
-        <App />
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+        <ThemeProvider theme={theme.darkTheme}>
+            <ReactQueryDevtools initialIsOpen={false} />
+            <App />
+        </ThemeProvider>
+    </QueryClientProvider>
     // </React.StrictMode>
 );
