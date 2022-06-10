cc0cc501ac9e19c6c8ebb4f15f6c05c92c81e6a5
kw22228 <hot5521@naver.com>
2022-06-10 18:04:05 +0900

    init: dartmode button

---
+++ coders/type-coin/src/components/DarkMode/DarkMode.tsx
@@ -0,0 +1,15 @@
+import DarkSwitch from '../../animation/darkSwitch.json';
+
+const lottieOptions = {
+    animationData: DarkSwitch,
+    loop: false,
+    autoplay: false,
+    rendererSettings: {
+        className: 'add-class', // svg에 적용
+        preserveAspectRatio: 'xMidYMid slice',
+    },
+};
+const DarkMode = () => {
+    return;
+};
+export default DarkMode;
