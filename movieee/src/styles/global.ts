c5ae1cac426bbf57d1f3329d4f4c4bffda3c8f19
kw22228 <hot5521@naver.com>
2022-06-23 17:59:59 +0900

    init: global style

---
+++ movieee/src/styles/global.ts
@@ -0,0 +1,13 @@
+import { createGlobalStyle } from 'styled-components';
+import { reset } from 'styled-reset';
+
+const GlobalStyle = createGlobalStyle`
+    ${reset}
+
+    a{
+        color: inherit;
+        text-decoration:none;
+    }
+`;
+
+export default GlobalStyle;
