727f664002fc194999d9b2614704d9d6e648b7f4
kw22228 <hot5521@naver.com>
2022-06-23 17:59:49 +0900

    init: router page

---
+++ movieee/src/pages/Main.tsx
@@ -0,0 +1,14 @@
+const Main = () => {
+    return (
+        <div
+            style={{
+                backgroundColor: 'white',
+                height: '200vh',
+            }}
+        >
+            Home
+        </div>
+    );
+};
+
+export default Main;
---
+++ movieee/src/pages/index.ts
@@ -0,0 +1,3 @@
+export { default as Main } from './Main';
+export { default as Tv } from './Tv';
+export { default as Search } from './Search';
