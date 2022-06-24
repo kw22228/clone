2d59449b99c279093c50b827bbceeb391d274265
kw22228 <hot5521@naver.com>
2022-06-24 18:08:09 +0900

    add: global style font

--- movieee/src/styles/global.ts
+++ movieee/src/styles/global.ts
@@ -4,6 +4,18 @@ import { reset } from 'styled-reset';
 const GlobalStyle = createGlobalStyle`
     ${reset}
 
+    @import url('https://fonts.googleapis.com/css2?family=Source+Sans+Pro:ital,wght@1,300&display=swap');
+
+    *{
+        box-sizing: border-box;
+    }
+    body{
+        font-weight: 300;
+        font-family: 'Source Sans Pro', sans-serif;
+        color: ${props => props.theme.white.darker};
+        background-color: black;
+    }
+
     a{
         color: inherit;
         text-decoration:none;
