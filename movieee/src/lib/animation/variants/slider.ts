a679e48e6e7ce3670917609f5dc3e0bb79aaeb04
kw22228 <hot5521@naver.com>
2022-06-28 17:44:22 +0900

    add: slider variants

--- movieee/src/lib/animation/variants/slider.ts
+++ movieee/src/lib/animation/variants/slider.ts
@@ -11,3 +11,28 @@ export const rowVariants: Variants = {
         x: -domWidth,
     }),
 };
+
+export const boxVariants: Variants = {
+    normal: {
+        scale: 1,
+        type: 'tween',
+    },
+    hover: {
+        scale: 1.3,
+        y: -100,
+        transition: {
+            delay: 0.5,
+            type: 'tween',
+        },
+    },
+};
+
+export const infoVariants: Variants = {
+    hover: {
+        opacity: 1,
+        transition: {
+            delay: 0.5,
+            type: 'tween',
+        },
+    },
+};
