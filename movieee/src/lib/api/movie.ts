138e8b70774b7ce9d2fee6205c835a2bb803a071
kw22228 <hot5521@naver.com>
2022-06-28 17:44:08 +0900

    add: async delay

--- movieee/src/lib/api/movie.ts
+++ movieee/src/lib/api/movie.ts
@@ -1,12 +1,16 @@
 import axios from 'axios';
 import getQuery from '../utils/getQuery';
+import delay from '../utils/promiseDelay';
 
 interface IAxios<T> {
     data: T;
 }
+
 const MovieAPI = {
     all: async <T>(path: string, quries: string[] = []): Promise<T> => {
         const url = getQuery(path, quries);
+        await delay(1200);
+
         const { data }: IAxios<T> = await axios.get(url);
 
         return data;
